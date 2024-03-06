494d1717097363b29c8d57aa18caa903cb3a8fb3
Thiago Frinhani <thiago.frinhani@ths.inf.br>
2024-03-06 00:54:15 -0300

    Ajuste home

--- app/src/components/pages/Home.js
+++ app/src/components/pages/Home.js
@@ -2,11 +2,9 @@ function Home() {
     return (
         <section>
             <h1>Bem vindo ao Payments!</h1>
-            <div className="presentation">
-                <h4>Este sistema te permite cadastrar saldos e pagamentos.</h4>
-                <h4> Cada pagamento pode consumir parte de um saldo cadastrado,</h4>
-                <h4>ou até o valor total de um saldo, desde que este saldo tenha valor restante.</h4>
-            </div>
+            <h4>Este sistema te permite cadastrar saldos e pagamentos.</h4>
+            <h4> Cada pagamento pode consumir parte de um saldo cadastrado,</h4>
+            <h4>ou até o valor total de um saldo, desde que este saldo tenha valor restante.</h4>
         </section >
     )
 }
